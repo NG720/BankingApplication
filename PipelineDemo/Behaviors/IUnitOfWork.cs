1ad128818c80fe757567c5b95b82356c82c54278
NG720 <69837791+NG720@users.noreply.github.com>
2026-05-08 22:43:15 +0300

    Delete PipelineDemo/Behaviors/IUnitOfWork.cs

--- PipelineDemo/Behaviors/IUnitOfWork.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore.Storage;
-
-namespace PipelineDemo.Interfaces;
-
-public interface IUnitOfWork
-{
-    Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken cancellationToken = default);
-    Task CommitAsync(CancellationToken cancellationToken = default);
-}
