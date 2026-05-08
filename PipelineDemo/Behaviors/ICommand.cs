d1531206544c7eb88e24aaf35d3419348439bacb
NG720 <69837791+NG720@users.noreply.github.com>
2026-05-08 22:43:07 +0300

    Delete PipelineDemo/Behaviors/ICommand.cs

--- PipelineDemo/Behaviors/ICommand.cs
+++
@@ -1,6 +0,0 @@
-using MediatR;
-
-namespace PipelineDemo.Interfaces;
-
-// Command'ları Query'lerden ayırt etmek için marker interface
-public interface ICommand<TResponse> : IRequest<TResponse> { }
